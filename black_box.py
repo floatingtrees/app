6d153993eac8036033e60569e960fda6843a9ecb
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2024-02-18 08:58:41 -0800

    tested hop-skip-jump

--- black_box.py
+++ black_box.py
@@ -1,10 +1,12 @@
 import torch
 import math
 
+
 class HopSkipJump:
 	def __init__(self, model_query_fn, clip_min = None, clip_max = None, input_shape = torch.tensor([3, 224, 224]), targeted = False):
 		self.model_query_fn = model_query_fn
 		self.total_classes = 1000
+		self.input_shape = input_shape
 		self.targeted = targeted
 		self.theta = 0.01 / (torch.sqrt(torch.prod(input_shape)))
 		self.clip_min = clip_min
@@ -19,20 +21,25 @@ class HopSkipJump:
 		current_image = initial_input
 		for i in range(max_iter):
 			delta = self._compute_delta(current_image, adversarial_sample, first_iter)
-			current_sample = self._clip(self._binary_search(current_image, initial_input, delta, target))
-			dist = torch.linalg.norm(current_sample - initial_input)
-			grads = self._approximate_gradient(current_image, initial_input, num_evals, target)
+			current_image = self._clip(self._binary_search(current_image, initial_input, delta, target))
+
+			dist = torch.linalg.norm(current_image - initial_input)
+			grads = self._approximate_gradient(current_image, initial_input, num_evals, target, delta)
+			epsilon = self._geometric_progression(current_image, grads, dist, num_evals, delta, i, target)
+			current_image = self._clip(current_image + epsilon * grads)
+			current_image = self._binary_search(current_image, initial_input, delta, target)
 
 
 			first_iter = False
+		return current_image
 
-	def _approximate_gradient(current_image, initial_input, num_evals, target):
+	def _approximate_gradient(self, current_image, initial_input, num_evals, target, delta):
 		shape = list(current_image.size())
 		shape[0] = num_evals
 		noise = torch.randn(shape)
-		perturbed_sample = self._clip(current_sample.clone().detach() + delta * noise)
-		noise = (perturbed_sample - current_sample) / delta
-		decisions = self._validate_sample(perturbed)
+		perturbed_sample = self._clip(current_image.clone().detach() + delta * noise)
+		noise = (perturbed_sample - current_image) / delta
+		decisions = self._validate_sample(perturbed_sample)
 		output_shape = decisions.size()
 
 		fval = 2 * torch.reshape(decisions.float(), output_shape) - 1.0
@@ -47,8 +54,8 @@ class HopSkipJump:
 			fval = fval.detach().clone() - torch.mean(fval)
 		gradf = torch.mean(fval * noise, dim = 0)
 
-	gradf = gradf / (torch.linalg.norm(gradf) + self.stabilizer)
-	return gradf
+		gradf = gradf / torch.linalg.norm(gradf)
+		return gradf
 
 
 	def _compute_delta(self, current_image, initial_input, first_iter):
@@ -62,10 +69,10 @@ class HopSkipJump:
 		au = 1
 		al = 0 
 		alpha = 0.5
-		while math.abs(au - al) < self.theta:
-			alpha = (au - al) / 2
+		while abs(au - al) > self.theta:
+			alpha = (au + al) / 2
 			perturbed_sample = self._clip((1 - alpha) * initial_input + alpha * current_image)
-			model_output = self._validate_sample(adjusted_input)
+			model_output = self._validate_sample(perturbed_sample)
 			if model_output == 1:
 				self.cached_result = perturbed_sample
 				au = alpha
@@ -75,8 +82,23 @@ class HopSkipJump:
 		return self.cached_result
 
 
+	def _geometric_progression(self, current_image, grads, dist, num_evals, delta, i, target):
+		epsilon = dist.clone().detach() / math.sqrt(i)
 
+		def phi(epsilon):
+			new = self._clip(current_image + epsilon * grads)
 
+			success = self._validate_sample(new)
+			return success
+
+		iterations = 10
+		while not phi(epsilon):
+			iterations -= 1
+			epsilon /= 2
+			if iterations == 0:
+				break
+
+		return epsilon
 
 	def _validate_sample(self, adjusted_input):
 		model_output = self.model_query_fn(adjusted_input)
--- black_box_tests.py
+++ black_box_tests.py
@@ -1,8 +1,12 @@
 import torch
 import math
-import HSJA
+import black_box
+from PIL import Image
+from torchvision import transforms
 
-class model_query_fn(model):
+
+
+class model_query_fn:
 	def __init__(self, model):
 		self.model = model
 		self.model.eval()
@@ -14,3 +18,18 @@ class model_query_fn(model):
 
 model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
 model.eval()
+image = Image.open("rabbit.png")
+
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+image = torch.unsqueeze(preprocess(image), 0)
+adv_image = Image.open("samoyed.jpg")
+adv_image = torch.unsqueeze(preprocess(adv_image), 0)
+
+
+attack = black_box.HopSkipJump(model_query_fn(model), clip_min = -2.5, clip_max = 2.5, targeted = True)
+image = attack.generate(image, max_iter = 10000, num_evals = 64, target = 331, adversarial_sample = adv_image)
